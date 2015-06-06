b6a2451018966bc8b5fdd20a6111b940ad0396cd
Niko Nevala <niko@nevala.fi>
2015-06-06 13:06:34 +0100

    Set up extension to load CSS and JS assets from a separate server

--- chrome/bootstrap.js
+++ chrome/bootstrap.js
@@ -1,19 +1,80 @@
 console.log('NAAB activated');
 
-setTimeout(function() {
-  var script = document.createElement('script');
-  script.src = chrome.extension.getURL('js/naab.js');
-  (document.head||document.documentElement).appendChild(script);
+chrome.storage.sync.get({
+  mode: 'live',
+  liveUrl: 'https://s3-eu-west-1.amazonaws.com/naab.idleplaythings.com',
+  devUrl: 'http://127.0.0.1:3000'
+}, function(options) {
+  var BASE_URL, RELEASE_URL, CSS_URL, JS_URL;
 
-  script = document.createElement('script');
-  script.src = chrome.extension.getURL('js/init.js');
-  (document.head||document.documentElement).appendChild(script);
+  if (options.mode) {
+    BASE_URL = options.liveUrl;
+    RELEASE_URL = BASE_URL + '/RELEASE';
+    CSS_URL = BASE_URL + '/%1/src/css/naab.css';
+    JS_URL = BASE_URL + '/%1/src/js/naab.js';
+  } else {
+    BASE_URL = options.devUrl;
+    RELEASE_URL = BASE_URL + '/RELEASE';
+    CSS_URL = BASE_URL + '/src/css/naab.css';
+    JS_URL = BASE_URL + '/src/js/naab.js';
+  }
 
+  /**
+   * The bootstrap script runs in an execution environment separate from the actual browser window,
+   * so Moo Tools etc. aren't available here. Hence the oldskool approach.
+   *
+   * The actual NAAB scripts are served from Amazon S3.
+   *
+   * First request to the RELEASE file determines which version of NAAB should be injected. This approach
+   * makes it very easy and fast to deploy changes (read: fixes) to NAAB, and to also control the current
+   * version in case it needs to be rolled back and forth.
+   */
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4 || xhr.status != 200) {
+      return;
+    }
+
+    addCss(formatString(CSS_URL, [xhr.responseText]));
+    addScript(formatString(JS_URL, [xhr.responseText]), function() {
+      // Again, this execution context is separate from the one actually used by the ARMY5 frame,
+      // so NAAB initialisation must happen via a script that's injected to the actual page.
+      addScript(chrome.extension.getURL('init.js'));
+    });
+  }
+  xhr.open('GET', RELEASE_URL, true);
+  xhr.send();
+})
+
+function formatString(string, replacements) {
+  return string.replace(/(%\d+)/g, substitute(replacements));
+};
+
+function substitute(substitutions) {
+  if (typeof substitutions === 'undefined' || !substitutions) {
+    substitutions = [];
+  }
+
+  return function(match) {
+    return substitutions[parseInt(match.substr(1) - 1, 10)];
+  };
+};
+
+function addCss(href) {
   var link = document.createElement('link');
-  link.href = chrome.extension.getURL('css/naab.css');
+  document.documentElement.appendChild(link);
   link.type = 'text/css';
   link.rel = 'stylesheet';
-  (document.head||document.documentElement).appendChild(link);
+  link.href = href;
+}
 
-  console.log('NAAB loaded');
-}, 1000);
+function addScript(src, onload) {
+  var script = document.createElement('script');
+  document.head.appendChild(script);
+
+  if (onload) {
+    script.onload = onload;
+  }
+
+  script.src = src;
+}
---
+++ chrome/options.js
@@ -0,0 +1,33 @@
+function saveOptions() {
+  chrome.storage.sync.set({
+    mode: document.getElementById('mode').value,
+    urls: {
+      live: document.getElementById('liveUrl').value,
+      dev: document.getElementById('devUrl').value
+    }
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restoreOptions() {
+  chrome.storage.sync.get({
+    mode: 'live',
+    liveUrl: 'https://s3-eu-west-1.amazonaws.com/naab.idleplaythings.com',
+    devUrl: 'http://127.0.0.1:3000'
+  }, function(options) {
+    document.getElementById('mode').value = options.mode;
+    document.getElementById('liveUrl').value = options.liveUrl;
+    document.getElementById('devUrl').value = options.devUrl;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
